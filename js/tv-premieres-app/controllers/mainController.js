c005dfc32defa96a551ea5f0dad5673c3edad3ee
Connor James Leech <connorleech@gmail.com>
2013-12-19 18:32:03 +0300

    add genre filter

--- js/tv-premieres-app/controllers/mainController.js
+++ js/tv-premieres-app/controllers/mainController.js
@@ -2,6 +2,9 @@ app.controller("mainController", function($scope, $http){
     
     $scope.apiKey = '3aac6b6682740a17bec433abdf6d7537';
     $scope.results = [];
+    $scope.filterText = null;
+    $scope.genreFilter = null;
+    $scope.availableGenres = [];
     $scope.init = function() {
         // API requires a start date
         var today = new Date();
@@ -17,10 +20,20 @@ app.controller("mainController", function($scope, $http){
                     // the API stores the date separately for each episode, save it to use later
                     var date = day.date;
                     
-                    // add each episode to the results array
+                    // loop through day's episodes
                     angular.forEach(day.episodes, function(episode, index){
                         episode.date = date;
-                        $scope.results.push(episode);
+                        $scope.results.push(episode); // add episode to results array
+                        
+                        //loop through episode's genres
+                        angular.forEach(episode.show.genres, function(genre, index){
+                            var inAG = false;
+                            angular.forEach($scope.availableGenres, function(ag, index){
+                                if (ag == genre){ inAG = true; }
+                            });
+                            if (!inAG) { $scope.availableGenres.push(genre); }
+                                
+                        });
                     });
                 });            
             
